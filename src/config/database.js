1e30a24f469668edb2d16cd5079586957f4dd2a5
Sam Lopez <samdl8116@gmail.com>
2026-07-14 16:52:45 -0600

    Add DNS import and commented-out server configuration in database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 import config from './config.js';
+import dns from "node:dns/promises";
 
 const connectDB = async () => {
     try {
+        //dns.setServers(["1.1.1.1", "8.8.8.8"]);
         const conn = await mongoose.connect(config.mongoUrl, {
             serverSelectionTimeoutMS: 5000,
         });
