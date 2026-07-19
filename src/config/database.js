06d4b3e716237e4a19d0fa92f3c9474e72ad220d
Sam Lopez <samdl8116@gmail.com>
2026-07-18 18:16:37 -0600

    Increase MongoDB server selection timeout and enable custom DNS servers

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,9 @@ import dns from "node:dns/promises";
 
 const connectDB = async () => {
     try {
-        //dns.setServers(["1.1.1.1", "8.8.8.8"]);
+        dns.setServers(["1.1.1.1", "8.8.8.8"]);
         const conn = await mongoose.connect(config.mongoUrl, {
-            serverSelectionTimeoutMS: 5000,
+            serverSelectionTimeoutMS: 20000,
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
